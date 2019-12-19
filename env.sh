47843b9dab14af58706fa132835e81b56ff54ba4
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-12-19 12:56:00 +0530

    set Env variables for linux

---
+++ env.sh
@@ -0,0 +1,10 @@
+export PHASE=developement
+export enc_key=FZDHuXvT2bSp5Mh4aDxUGFtHpnnnOOAa3WIDTMfuqV@!_MMaa=
+export jwt_sec=ttJJHSH_2NNDH8V@1ndjddd_kk-dky89lddowbddddvdiwodmd=
+export port=8029
+export database=c5c6f6e057999f15f9eb227c8634321c
+export user=5d672a485b0bdd117693d43bb10f885c
+export password=
+export host=2fc836689def984232b21aee6a436259
+export sendermail=1ebb60c808b59998d46abad38f0d365dd63fae413aec8858d3e0dd3bae712db9
+export senderpass=e262d0bde6f53a85d92f48d319c6df9a0dd52899d720ba821b4fc5ab27a77fd8
